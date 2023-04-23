d25e5f1480236a2b6062d9b9c0302cb2e2212441
George Raafat <georgexraafat@gmail.com>
2023-04-22 18:01:03 -0700

    a

---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	printf("%s\n", NULL);
+	return (0);
+}
---
+++ test/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int p_char(char c);
+int p_string(char *s);
+
+#endif
